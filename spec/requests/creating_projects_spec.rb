ddcea3524b50475a54d65007d6f0d6fbf1908f00
Lucas Lacerda Gertel <lucas.gertel@realwebtrade.com.br>
2013-01-29 17:22:41 -0200

    Add title functionality for show page

--- spec/requests/creating_projects_spec.rb
+++ spec/requests/creating_projects_spec.rb
@@ -8,5 +8,10 @@ feature 'Creating Projects' do
 	  fill_in 'project_description', :with => "A text-editor for OS X"
 	  click_button 'submit_project'
 	  page.should have_content('Project has been created')
+
+	  project = Project.find_by_name('TextMate 2')
+	  page.current_url.should == project_url(project)
+	  title = 'TextMate 2 - Projects - Ticketee'
+	  find('title').should have_content(title)
 	end 
 end
