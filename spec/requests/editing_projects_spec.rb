5d974d7dbdbf9ad00357b765b770662b9ce582b8
Lucas Lacerda Gertel <lucas.gertel@realwebtrade.com.br>
2013-01-29 18:48:47 -0200

    Add updating projects functionality

---
+++ spec/requests/editing_projects_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+feature "Editing Projects" do
+
+	before do
+		Factory(:project, name:'TextMate 2')
+		visit '/'
+		click_link 'TextMate 2'
+		click_link 'Edit Project'
+	end
+
+	scenario "Updating a project" do
+		fill_in 'Name', with: 'TextMate2 beta'
+		click_button 'Update Project'
+		page.should have_content('Project has been updated')
+	end
+
+	scenario "Updating a project with invalid attributes" do
+		fill_in 'Name', with: ''
+		click_button "Update Project"
+		page.should_not have_content('Project has been updated')
+	end
+
+end
