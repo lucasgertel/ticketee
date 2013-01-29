17ea9df9381d22615d6941e765eee2f28f40d82f
Lucas Lacerda Gertel <lucas.gertel@realwebtrade.com.br>
2013-01-29 19:29:25 -0200

    Implement delete functionality for projects

---
+++ spec/requests/deleting_projects_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+feature "Deleting Projects" do
+  
+  scenario "Deleting a project" do
+  	Factory(:project, name: 'TextMate2')
+  	visit '/'
+  	click_link 'TextMate2'
+  	click_link 'Delete Project'
+  	page.should have_content('Project has been deleted')
+  end
+end
