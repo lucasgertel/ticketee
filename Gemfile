ad63e87f83e170a775bd08eb0131b98aa4fe4f62
Lucas Lacerda Gertel <lucas.gertel@realwebtrade.com.br>
2013-01-31 17:08:46 -0200

    Add feature to ensure signup is always working

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ gem 'rails', '3.2.11'
 gem 'faker', '1.0.1'
 gem 'jquery-rails', '2.0.2'
 gem 'bootstrap-sass', '2.1'
+gem 'devise'
 
 group :development, :test do
   gem 'sqlite3', '1.3.5'
--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,8 @@ module Ticketee
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Devise configuration
+    config.assets.initialize_on_precompile = false
   end
 end
